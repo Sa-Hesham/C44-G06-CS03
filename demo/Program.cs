f3c1b9ce98a7756e15f12c9e0a5ac5d7bbd3ca1a
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-06-29 15:30:49 +0300

    Fractions And discards

--- demo/Program.cs
+++ demo/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Fractions And discards
+            //// by defualt  decimal numbers is d ouble 
+            //double  d =3.5; // 8 byte  15 or 16 digit after  Decimal point
+            //float a = 3.3f; //4 byt  7 digit after  Decimal point
+            //decimal d = 3.3m; //16 byte  28-29 digit after  Decimal point
+
+           
+            //there are many ways  to formating numbers
+            
+            //currency formating 
+            //double n =45454.34554676;
+            //Console.WriteLine( "n= {0:c1}", n);
+            //Console.WriteLine($"n= {n:c1}");
+            //Console.WriteLine(n.ToString("c3"));
+
+
+
+
+
+
+            #endregion
         }
     }
 }
