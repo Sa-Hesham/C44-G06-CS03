2149439941de47c1c10d5dcfab86f25608834401
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-06-29 16:07:31 +0300

    Emplicit &Explicit

--- demo/Program.cs
+++ demo/Program.cs
@@ -10,9 +10,9 @@
             //float a = 3.3f; //4 byt  7 digit after  Decimal point
             //decimal d = 3.3m; //16 byte  28-29 digit after  Decimal point
 
-           
+
             //there are many ways  to formating numbers
-            
+
             //currency formating 
             //double n =45454.34554676;
             //Console.WriteLine( "n= {0:c1}", n);
@@ -25,6 +25,61 @@
 
 
             #endregion
+
+            #region Implicit and Explicit-Casting
+            // there is two types of casting 
+            //implicit the compiler  casting automaticly from small data type to big data type
+            //but can not convert from small to  big data type because c#  type save 
+            //examp
+
+            //int x = 5000000;
+            //double z = x;
+            //Console.WriteLine(z);
+
+            // but in Explicit  user will convert data type for other data type   
+
+            //long a= 100;
+            //int x =  (int)a;
+            //Console.WriteLine(x);
+            // can not convert type implicit  
+            // in this case  need  Explicit casting 
+
+            //  checked
+            //{
+
+
+
+            //      long a = 1007585;
+            //      int x = (int)a;
+            //      Console.WriteLine(x);
+
+
+            //  }
+
+            // if  the value in long is bigger than int (max vlaue ) 
+            //will be occured data loss (overflowing)
+            // to handle this errors  used chaked
+
+            //long a = 1007585655757575;
+
+
+            //if (a > int.MaxValue || a < int.MinValue)
+            //{
+
+            //    Console.WriteLine("you cant Explicit (over flowing)");
+            //}
+            //else
+            //{
+            //    int x = (int)a;
+            //    Console.WriteLine(x);
+            //}
+           
+
+
+
+
+            #endregion
+
         }
     }
 }
