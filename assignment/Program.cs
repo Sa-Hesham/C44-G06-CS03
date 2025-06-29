10fe916c69e54900cfb3f183733f730a9e2e3981
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-06-29 22:31:35 +0300

    Question_3

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -35,7 +35,7 @@ namespace assignment
             //Write C# program that converts a string to an integer, but the string contains non-numeric characters.
             //   And mention what will happen 
 
-            string s1 = "123dsrr";
+            //string s1 = "123dsrr";
             //// if we used the convert it will be throw error because the string is invalid to convert 
             //int num = Convert.ToInt32(s1);
 
@@ -57,6 +57,43 @@ namespace assignment
 
             #endregion
 
+
+
+
+
+            #region Q3
+
+            //Write C# program that Perform a simple arithmetic operation with
+            //    floating-point numbers And mention what will happen
+
+            // we can also using double datatype insead of  float 
+            // because double [15-16 after precition] More accurate than float [7 after precition] 
+            // decimal is More accurate than [28 ,29 digit]  after precition
+            Console.WriteLine("Enter the first number");
+
+            float.TryParse(Console.ReadLine(),out float num1);
+
+
+            Console.WriteLine("Enter the second number");
+
+            float.TryParse(Console.ReadLine(), out float num2);
+
+            float result = (num1 + num2);
+
+            //Console.WriteLine($"sum = {result}");
+
+
+            //Console.WriteLine($"sum = { (double)result}");
+
+            //Console.WriteLine($"sum = {(decimal)result}");
+
+
+
+
+
+
+
+            #endregion
 
 
 
