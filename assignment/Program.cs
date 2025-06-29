d3a818a46b20b478c0adeaff239e90fcd96a7d94
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-06-29 21:31:59 +0300

    Question_1

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace assignment
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+
+namespace assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+
+            //Write a program that allows the user to enter a number then print it.
+
+            Console.WriteLine("please enter number ");
+           bool isparse= int.TryParse(Console.ReadLine(),out int num);
+            Console.Clear();
+            if (isparse)   //or isparse==true
+            {
+                Console.WriteLine($"the number that you enterd is : {num}");
+            }
+            else
+            {
+                Console.WriteLine($"the number is invalid  We add the default value {num}");
+            }
+           
+            
+            
+            
+            
+            #endregion
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
